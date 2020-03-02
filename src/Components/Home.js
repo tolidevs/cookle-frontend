d743104ec437fe1c17458386a206ec8317a8ed64
flatironschool <t.benson@live.co.uk>
2020-03-02 18:50:24 +0000

    creating search form

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,7 @@ import { Header, Segment, Sidebar, Container } from 'semantic-ui-react';
 import LoginButton from '../Components/LoginButton';
 import LoginForm from '../Components/LoginForm';
 import UserMenu from '../Components/UserMenu';
+import SearchForm from './SearchForm'
 
 class Home extends React.Component {
 	state = {
@@ -28,36 +29,39 @@ class Home extends React.Component {
 
 	setUser = data => {
 		this.setState({
-			currentUser: data.user
+			currentUser: data.email
 		});
 	};
 
 	loginFunction = (email, password) => {
 		const data = { email, password };
-		fetch('https://localhost:3000', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
-			.then(res => res.json)
-			.then(this.setUser)
-			.catch(console.log);
+		this.setUser(data)
+
+		// fetch('https://localhost:3000/users/login', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+		// 	body: JSON.stringify(data)
+		// })
+		// 	.then(res => res.json)
+		// 	.then(this.setUser)
+		// 	.catch(console.log);
 	};
 
 	signUpFunction = (email, password) => {
 		const data = { email, password };
-		fetch('https://localhost:3000', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
-			.then(res => res.json)
-			.then(this.setUser)
-			.catch(console.log);
+		this.setUser(data)
+	// 	fetch('http://localhost:3000/users/create', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify(data)
+	// 	})
+	// 		.then(res => res.json)
+	// 		.then(this.setUser)
+	// 		.catch(console.log);
 	};
 
 	render() {
@@ -80,18 +84,7 @@ class Home extends React.Component {
 								displayUserMenu={this.displayUserMenu}
 							/>
 							<Container>
-								<p>
-									Lorem Ipsum is simply dummy text of the printing and
-									typesetting industry. Lorem Ipsum has been the industry's
-									standard dummy text ever since the 1500s, when an unknown
-									printer took a galley of type and scrambled it to make a type
-									specimen book. It has survived not only five centuries, but
-									also the leap into electronic typesetting, remaining
-									essentially unchanged. It was popularised in the 1960s with
-									the release of Letraset sheets containing Lorem Ipsum
-									passages, and more recently with desktop publishing software
-									like Aldus PageMaker including versions of Lorem Ipsum.
-								</p>
+								<SearchForm />
 							</Container>
 						</Segment>
 					</Sidebar.Pusher>
