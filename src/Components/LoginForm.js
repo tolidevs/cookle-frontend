e06bf50793417c542f17faa1f12c3784f84aac0d
RogeredBacon <jinglis12@googlemail.com>
2020-03-05 17:14:51 +0000

    search functionality

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,52 +1,55 @@
 import React from 'react';
 import { Button, Form, Segment, Sidebar } from 'semantic-ui-react';
 
-
-
 const LoginForm = props => {
-	const { loginFunction, signUpFunction, displayLogin, loginShown } = props;
+	const { loginFunction, displayLogin, loginShown } = props;
 	return (
 		<Sidebar
-		as={Segment}
-		animation="overlay"
-		icon="labeled"
-		inverted
-		onHide={displayLogin}
-		direction={"top"}
-		visible={loginShown}
-		width="thin"
-		color="teal"
-		>
-		<Segment>
-			<Form
-			className="login-form"
-			onSubmit={e =>
-				loginFunction( e, e.target.email.value, e.target.password.value)
-			}
-			>
-			<Form.Group widths={2}>
-				<Form.Input
-				name="email"
-				label="Email"
-				placeholder="Email Address"
-				type="email"
-				required
-				/>
-				<Form.Input
-				name="password"
-				label="Password"
-				placeholder="Password"
-				type="password"
-				required
-				/>
-			</Form.Group>
+			as={Segment}
+			animation='overlay'
+			icon='labeled'
+			inverted
+			onHide={displayLogin}
+			direction={'top'}
+			visible={loginShown}
+			width='thin'
+			color='teal'>
+			<Segment>
+				<Form
+					className='login-form'
+					onSubmit={e =>
+						loginFunction(
+							e,
+							e.target.email.value,
+							e.target.password.value,
+							document.activeElement
+						)
+					}>
+					<Form.Group widths={2}>
+						<Form.Input
+							name='email'
+							label='Email'
+							placeholder='Email Address'
+							type='email'
+							required
+						/>
+						<Form.Input
+							name='password'
+							label='Password'
+							placeholder='Password'
+							type='password'
+							required
+						/>
+					</Form.Group>
 
-			<Button name="login" type="submit">
-				Login
-			</Button>
-			{/* <Button name="signup" type="submit" secondary onClick={e => console.log(e.target)}>Sign Up</Button> */}
-			</Form>
-		</Segment>
+					<Button name='login' type='submit'>
+						Login
+					</Button>
+					<Button name='create' type='submit' secondary>
+						Sign Up
+					</Button>
+				</Form>
+			</Segment>
 		</Sidebar>
 	);
 };
